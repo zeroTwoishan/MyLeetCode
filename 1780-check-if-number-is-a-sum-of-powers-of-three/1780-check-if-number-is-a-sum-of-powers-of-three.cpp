4ef1301da1724e0894c0ebb639596cff3fa6b3d5
zeroTwoishan <ivankaishan09092007@gmail.com>
2026-07-02 00:52:58 +0530

    Time: 0 ms (100%), Space: 7.7 MB (92.95%) - LitGit

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while(n > 0){
+            int r = n % 3;
+            if(r == 2) return false;
+            n = n/3;
+        }
+        return true;
+    }
+};
