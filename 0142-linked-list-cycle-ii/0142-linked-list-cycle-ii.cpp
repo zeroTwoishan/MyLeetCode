f78bee44d55e32fc7946aafb50a1931bf5985d37
zeroTwoishan <ivankaishan09092007@gmail.com>
2026-07-02 00:43:44 +0530

    Time: 6 ms (72.47%), Space: 11.3 MB (54.44%) - LitGit

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == nullptr || head->next == nullptr) return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
