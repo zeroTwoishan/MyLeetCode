f251536c8190fe944a0b97a57fbc98a9e148b0e3
zeroTwoishan <ivankaishan09092007@gmail.com>
2026-07-02 00:49:47 +0530

    Time: 0 ms (100%), Space: 19.8 MB (23.44%) - LitGit

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int low = 0;
+        int high = letters.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(letters[mid] <= target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return (low >= letters.size()) ? letters[0] : letters[low];
+    }
+};
