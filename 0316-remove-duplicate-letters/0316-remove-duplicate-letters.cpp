c23ee0e0ead86f5a67650e0ffca05a7bea3981b4
zeroTwoishan <ivankaishan09092007@gmail.com>
2026-07-02 16:33:04 +0530

    Time: 0 ms (100%), Space: 9.1 MB (83.16%) - LitGit

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -2,6 +2,34 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
         int n = s.size();
+        int lastIdx[26] = {0};
+        for(int i = 0; i < n; i++){
+            lastIdx[s[i] - 'a'] = i;
+        }
+
+        bool inStack[26] = {false};
+        string st;
+
+        for(int i = 0; i < n; i++){
+            char c = s[i];
+            if(inStack[c - 'a']) continue;
+
+            while(!st.empty() && c < st.back() && i < lastIdx[st.back() - 'a']){
+                inStack[st.back() - 'a'] = false;
+                st.pop_back();
+            }
+            st.push_back(c);
+            inStack[c - 'a'] = true;
+        }
+
+        return st; 
+    }
+};
+/*
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
         string ans = "";
         unordered_map<char,int>mpp(n);
         for(int i = 0; i < n; i++){
@@ -27,3 +55,4 @@ public:
         return ans;
     }
 };
+*/
