5bdce1a2c22e599c106fa5685106ebdece709502
zeroTwoishan <ivankaishan09092007@gmail.com>
2026-07-05 01:46:58 +0530

    Time: 0 ms (100%), Space: 10.8 MB (83.6%) - LitGit

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     string simplifyPath(string path) {
@@ -18,6 +19,39 @@ public:
         }
         string ans = "";
         for(auto& c : st){
+            ans += "/";
+            ans += c;
+        }
+        return ans.empty() ? "/" : ans;
+    }
+};
+*/
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        int n = path.size();
+        int i = 0;
+        
+        while (i < n) {
+            while (i < n && path[i] == '/') i++;  // skip slashes
+            if (i >= n) break;
+            
+            int start = i;
+            while (i < n && path[i] != '/') i++;  // read one segment
+            string c = path.substr(start, i - start);
+            
+            if (c == "..") {
+                if (!st.empty()) st.pop_back();
+            } else if (c == ".") {
+                continue;
+            } else {
+                st.push_back(c);
+            }
+        }
+        
+        string ans;
+        for (auto& c : st) {
             ans += "/";
             ans += c;
         }
