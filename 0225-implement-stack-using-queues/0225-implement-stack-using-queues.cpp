6cdaa17cbf1775786232f574ea37d81d12359c36
zeroTwoishan <ivankaishan09092007@gmail.com>
2026-08-03 01:09:56 +0530

    Time: 0 ms (100%), Space: 10 MB (74.89%) - LitGit

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,29 @@
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for (int i = 0; i < (int)q.size() - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
