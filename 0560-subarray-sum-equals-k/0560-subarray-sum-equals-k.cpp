657a6e2a001b3a040302833886524ffe01205990
zeroTwoishan <ivankaishan09092007@gmail.com>
2026-08-02 22:48:53 +0530

    Time: 56 ms (36.48%), Space: 51 MB (20.07%) - LitGit

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-    map<int,int> mpp;
+    unordered_map<int,int> mpp;
     int sum=0;
     int n=nums.size();
     int count=0;
