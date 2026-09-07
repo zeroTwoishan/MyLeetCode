7e02471c309f044c0ae29bfb5a8638360aa841e7
Ishan Keshari <ivankaishan09092007@gmail.com>
2026-09-07 12:34:26 +0530

    Time: 0 ms (100%), Space: 52.8 MB (98.38%) - LitGit

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int idx = abs(nums[i]) - 1;   // target index for this value
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];   // mark as "seen"
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) ans.push_back(i + 1);
+        }
+
+        return ans;
+    }
+};
+/*
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
         if(nums.empty()) return {};
         
         int n = nums.size();
@@ -16,3 +37,4 @@ public:
         return ans;
     }
 };
+*/
