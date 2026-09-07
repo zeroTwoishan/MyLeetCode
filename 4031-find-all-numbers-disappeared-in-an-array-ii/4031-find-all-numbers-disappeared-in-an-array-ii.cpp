63fefda63d2fd837b256d365aeda4b736a693db4
Ishan Keshari <ivankaishan09092007@gmail.com>
2026-09-07 12:59:02 +0530

    Time: 27 ms (99.91%), Space: 242.9 MB (51.02%) - LitGit

--- 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.cpp
+++ 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.cpp
@@ -3,33 +3,31 @@ public:
     vector<vector<int>> findDisappearedNumbers(vector<int>& nums, int lower, int upper) {
         vector<bool> present(upper + 1, false);
 
-        for(auto num : nums){
-            if(num <= upper)
-            present[num] = true;
+        for (int num : nums) {
+            if (num >= lower && num <= upper) {
+                present[num] = true;
+            }
         }
-        int i = lower;
 
         vector<vector<int>> ans;
 
-        while(i < upper + 1){
-            if(present[i]){
+        int i = lower;
+
+        while (i <= upper) {
+            if (present[i]) {
                 i++;
                 continue;
             }
-            vector<int> temp;
-            temp.push_back(i);
-            for(int j = i + 1; j < upper + 1; j++){
-                if(present[j]){
-                    temp.push_back(j - 1);
-                    break;
-                }
+
+            int start = i;
+
+            while (i <= upper && !present[i]) {
+                i++;
             }
-            if(temp.size() == 1){
-                temp.push_back(upper);
-            }
-            ans.push_back(temp);
-            i = (temp.back() + 1);
+
+            ans.push_back({start, i - 1});
         }
+
         return ans;
     }
 };
